3fe3e1ad42afc3347cf47fa074dce34015b5be3f
Rolando Lora <rolando.lora@gmail.com>
2013-07-10 12:56:29 -0400

    Adding index file

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
-var express = require('express');
+/* jshint indent: 2 */
+var express = require("express");
+var fs = require("fs");
+var app = express();
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get("/", function(request, response) {
+  var buffer = fs.readFileSync("index.html");
+  response.send(buffer.toString());
 });
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
